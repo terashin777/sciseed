3bbaa885df9cb18cfcaee6935420f45fc0312727
terashin777 <terashin0324@gmail.com>
2016-08-25 12:37:24 +0900

    タグ追加＃追加 タグ削除＃追加

--- userSearchController.js
+++ userSearchController.js
@@ -38,260 +38,271 @@ angular.module('concierAdminApp',[])
     $scope.asignee = localStorage.getItem("asignee");
         
     $http({
-                url: 'tag_list.json',
-                method:"GET",
-                datatype:"json"
-            }).
-            success(function(d, status, headers, config) {
+        url: 'tag_list.json',
+        method:"GET",
+        datatype:"json"
+    }).
+    success(function(d, status, headers, config) {
 
-                for(var tIdx in d){
-                    d[tIdx].categoryText = categoryMapper(d[tIdx].category);  //ユーザータグリストの中のcategoryを引数として渡し，それをswichで場合わけして，実際に表示する文字列に直している．
-                }
-
-                $scope.userTag = d; //ここにユーザータグが入る
-
-                $http({
-                    url: 'user_list.json',
-                    method: "GET",
-                    dataType: "json"
-                }).
-                    success(function(data, status, headers, config) {
-                        $scope.lineUserList = data; //ここにユーザーリストが入る
-                    }).
-                    error(function(data, status, headers, config) {
-                    });
-                }).
-                error(function(data, status, headers, config) {
-      
-        });
-
- $scope.getTagName = function(tagId){
-    for(var i in $scope.userTag){ //全タグリストをループし，引数として渡されたタグIDと一致するタグの名前を探す．
-        if($scope.userTag[i].id == tagId){
-            return $scope.userTag[i].name;
-        }
+    for(var tIdx in d){
+        d[tIdx].categoryText = categoryMapper(d[tIdx].category);  //ユーザータグリストの中のcategoryを引数として渡し，それをswichで場合わけして，実際に表示する文字列に直している．
     }
-    return "";
-  };
 
-  $scope.getTagId = function(tagName){
-    for(var i in $scope.userTag){ //全タグリストをループし，引数として渡されたタグ名と一致するタグIDを探す．
-        if($scope.userTag[i].name == tagName){
-            return $scope.userTag[i].id;
-        }
-    }
-    return ""; /*絞り込みで、全て表示を実装するには、ここはnullではなく、””でなくてはならない。下のフィルタの、if($scope.search.univ != "")での""と対応している。
-    両方ともnullにするという手も考えられるが、初期値が””なのでうまくいかない。初めの変数宣言で初期値をnullにすればnullでも問題ない。*/
-  };
+    $scope.userTag = d; //ここにユーザータグが入る
 
-$scope.filterUser = function(item) {
-    if(item.loyalty>=$scope.minLoyalty){
-        if($scope.serchQuery.type == "tag" && $scope.serchQuery.queryTag != ""){
-            var tagId = $scope.getTagId($scope.serchQuery.queryTag); //タグIDを取得する
-            if(tagId){
-                return item.user_tag.indexOf(tagId) != -1; //タグIDが初めに現れたインデックス番号を取得する
-            }else{
-                return false;
-            }
-        }else if($scope.serchQuery.type == "text" && $scope.serchQuery.queryText != ""){
-        return item.name.indexOf($scope.serchQuery.queryText)!=-1;         
-      }
-        return true;
-    }else{
-      return false
-    }
-};
-
-$scope.searchByTag = function(tagId){ //検索するタイプを指定し，serchQuery.queryTagに引数として渡されたタグIDからタグの名前を代入する
-    $scope.serchQuery.type = "tag";
-    $scope.serchQuery.queryTag = $scope.getTagName(tagId);
-};
-
-$scope.searchByText = function(){
-    $scope.serchQuery.type = "text";
-    $scope.serchQuery.queryText = $scope.userNameText;
-};
-
-$scope.minLoyaltyUpdate = function(){
-    $scope.minLoyalty = parseInt($scope.loyaltyStr);
-};
-
-$scope.doSearch = function() {
-    /*if($scope.user_univ.name == ""){
-        $scope.search.user_univ = "";
-        //$scope.search.user_univ = $scope.user_univ;
-    }else{
-        $scope.search.user_univ = $scope.user_univ.name;
-        //$scope.search.user_univ = $scope.user_univ;
-    }*/
-    $scope.search.univ        = $scope.getTagId($scope.selected.univ);
-    $scope.search.grade      = $scope.getTagId($scope.selected.grade);
-    $scope.search.preference = $scope.getTagId($scope.selected.preference);
-    $scope.search.major_art  = $scope.getTagId($scope.selected.major_art);
-    $scope.search.major_sci  = $scope.getTagId($scope.selected.major_sci);
-    $scope.search.industry   = $scope.getTagId($scope.selected.industry);
-    $scope.search.sex        = $scope.getTagId($scope.selected.sex);
-    $scope.search.operator   = $scope.getTagId($scope.selected.operator);
-    $scope.search.status     = $scope.getTagId($scope.selected.status);
-    $scope.search.loyalty    = $scope.selected.loyalty;
-     $scope.search.keyword        = $scope.selected.keyword;
-};
-
-$scope.filterByUniv = function(user) {
-    if($scope.search.univ != ""){ //選択されたタグが""（全て表示）でなければ絞り込みを行う．
-        return user.user_tag.indexOf($scope.search.univ) != -1; //array.indexOf(引数)はarrayに引数を含んでいればそのindex番号を返す．なければ-1を返す．
-    }else{
-        return -1; //filterを無効にしたいときは，戻り値に-1を指定すればよい。全て表示で用いる．
-    }
-};
-
-$scope.filterByGrade = function(user) {
-    if($scope.search.grade != ""){ //選択されたタグが""（全て表示）でなければ絞り込みを行う．
-        return user.user_tag.indexOf($scope.search.grade) != -1; //array.indexOf(引数)はarrayに引数を含んでいればそのindex番号を返す．なければ-1を返す．
-    }else{
-        return -1; //filterを無効にしたいときは，戻り値に-1を指定すればよい。全て表示で用いる．
-    }
-};
-
-$scope.filterByPreference = function(user) {
-    if($scope.search.preference != ""){ //選択されたタグが""（全て表示）でなければ絞り込みを行う．
-        return user.user_tag.indexOf($scope.search.preference) != -1; //array.indexOf(引数)はarrayに引数を含んでいればそのindex番号を返す．なければ-1を返す．
-    }else{
-        return -1; //filterを無効にしたいときは，戻り値に-1を指定すればよい。全て表示で用いる．
-    }
-};
-
-$scope.filterByMajorArt = function(user) {
-    if($scope.search.major_art != ""){ //選択されたタグが""（全て表示）でなければ絞り込みを行う．
-        return user.user_tag.indexOf($scope.search.major_art) != -1; //array.indexOf(引数)はarrayに引数を含んでいればそのindex番号を返す．なければ-1を返す．
-    }else{
-        return -1; //filterを無効にしたいときは，戻り値に-1を指定すればよい。全て表示で用いる．
-    }
-};
-
-$scope.filterByMajorSci = function(user) {
-    if($scope.search.major_sci != ""){ //選択されたタグが""（全て表示）でなければ絞り込みを行う．
-        return user.user_tag.indexOf($scope.search.major_sci) != -1; //array.indexOf(引数)はarrayに引数を含んでいればそのindex番号を返す．なければ-1を返す．
-    }else{
-        return -1; //filterを無効にしたいときは，戻り値に-1を指定すればよい。全て表示で用いる．
-    }
-};
-
-$scope.filterByIndustry = function(user) {
-    if($scope.search.industry != ""){ //選択されたタグが""（全て表示）でなければ絞り込みを行う．
-        return user.user_tag.indexOf($scope.search.industry) != -1; //array.indexOf(引数)はarrayに引数を含んでいればそのindex番号を返す．なければ-1を返す．
-    }else{
-        return -1; //filterを無効にしたいときは，戻り値に-1を指定すればよい。全て表示で用いる．
-    }
-};
-
-$scope.filterBySex = function(user) {
-    if($scope.search.sex != ""){ //選択されたタグが""（全て表示）でなければ絞り込みを行う．
-        return user.user_tag.indexOf($scope.search.sex) != -1; //array.indexOf(引数)はarrayに引数を含んでいればそのindex番号を返す．なければ-1を返す．
-    }else{
-        return -1; //filterを無効にしたいときは，戻り値に-1を指定すればよい。全て表示で用いる．
-    }
-};
-
-$scope.filterByOperator = function(user) {
-    if($scope.search.operator != ""){ //選択されたタグが""（全て表示）でなければ絞り込みを行う．
-        return user.user_tag.indexOf($scope.search.operator) != -1; //array.indexOf(引数)はarrayに引数を含んでいればそのindex番号を返す．なければ-1を返す．
-    }else{
-        return -1; //filterを無効にしたいときは，戻り値に-1を指定すればよい。全て表示で用いる．
-    }
-};
-
-$scope.filterByStatus = function(user) {
-    if($scope.search.status != ""){ //選択されたタグが""（全て表示）でなければ絞り込みを行う．
-        return user.user_tag.indexOf($scope.search.status) != -1; //array.indexOf(引数)はarrayに引数を含んでいればそのindex番号を返す．なければ-1を返す．
-    }else{
-        return -1; //filterを無効にしたいときは，戻り値に-1を指定すればよい。全て表示で用いる．
-    }
-};
-
-$scope.filterByLoyalty = function(user) {
-        return user.loyalty >= $scope.search.loyalty;
-};
-
-$scope.filterByKeyword = function(user) {
-     if($scope.search.status != ""){ //選択されたタグが""（全て表示）でなければ絞り込みを行う．
-          return $user == $scope.search.keyword; //array.indexOf(引数)はarrayに引数を含んでいればそのindex番号を返す．なければ-1を返す．
-    }else{
-        return -1; //filterを無効にしたいときは，戻り値に-1を指定すればよい。全て表示で用いる．
-    }
-};
-
- $scope.getUserMessages = function(user){
-
-    var url = LINE_API_URL+"/user/"+user.id+"/message";
     $http({
-      url: url,
-      method: "GET",
-      dataType: "json",
+        url: 'user_list.json',
+        method: "GET",
+        dataType: "json"
     }).
-    success(function(data, status, headers, config) {
-      for(var i in data){
-        data[i]['datetime'] = timeConverter(data[i]['created_date']);
+        success(function(data, status, headers, config) {
+        $scope.lineUserList = data; //ここにユーザーリストが入る
+    }).
+        error(function(data, status, headers, config) {
+    });
+    }).
+        error(function(data, status, headers, config) {
+
+    });
+
+    $scope.openTagAddField = function(user){
+        $scope.currentUser = user;
+        $scope.tag_add = true;
+
+    };
+
+    $scope.openTagRemoveField = function(user){
+        $scope.currentUser = user;
+        $scope.tag_add = false;
+    };
+
+    $scope.getTagName = function(tagId){
+        for(var i in $scope.userTag){ //全タグリストをループし，引数として渡されたタグIDと一致するタグの名前を探す．
+            if($scope.userTag[i].id == tagId){
+                return $scope.userTag[i].name;
+            }
+        }
+            return "";
+    };
+
+    $scope.getTagId = function(tagName){
+        for(var i in $scope.userTag){ //全タグリストをループし，引数として渡されたタグ名と一致するタグIDを探す．
+            if($scope.userTag[i].name == tagName){
+                return $scope.userTag[i].id;
+            }
+        }
+        return ""; /*絞り込みで、全て表示を実装するには、ここはnullではなく、””でなくてはならない。下のフィルタの、if($scope.search.univ != "")での""と対応している。
+        両方ともnullにするという手も考えられるが、初期値が””なのでうまくいかない。初めの変数宣言で初期値をnullにすればnullでも問題ない。*/
+      };
+
+    $scope.filterUser = function(item) {
+        if(item.loyalty>=$scope.minLoyalty){
+            if($scope.serchQuery.type == "tag" && $scope.serchQuery.queryTag != ""){
+                var tagId = $scope.getTagId($scope.serchQuery.queryTag); //タグIDを取得する
+                if(tagId){
+                  return item.user_tag.indexOf(tagId) != -1; //タグIDが初めに現れたインデックス番号を取得する
+                }else{
+                  return false;
+                }
+            }else if($scope.serchQuery.type == "text" && $scope.serchQuery.queryText != ""){
+              return item.name.indexOf($scope.serchQuery.queryText)!=-1;
+            }
+            return true;
+        }else{
+          return false
+        }
+    };
+
+    $scope.searchByTag = function(tagId){ //検索するタイプを指定し，serchQuery.queryTagに引数として渡されたタグIDからタグの名前を代入する
+        $scope.serchQuery.type = "tag";
+        $scope.serchQuery.queryTag = $scope.getTagName(tagId);
+    };
+
+    $scope.searchByText = function(){
+        $scope.serchQuery.type = "text";
+        $scope.serchQuery.queryText = $scope.userNameText;
+    };
+
+    $scope.minLoyaltyUpdate = function(){
+        $scope.minLoyalty = parseInt($scope.loyaltyStr);
+    };
+
+    $scope.doSearch = function() {
+        /*if($scope.user_univ.name == ""){
+            $scope.search.user_univ = "";
+            //$scope.search.user_univ = $scope.user_univ;
+        }else{
+            $scope.search.user_univ = $scope.user_univ.name;
+            //$scope.search.user_univ = $scope.user_univ;
+        }*/
+        $scope.search.univ        = $scope.getTagId($scope.selected.univ);
+        $scope.search.grade      = $scope.getTagId($scope.selected.grade);
+        $scope.search.preference = $scope.getTagId($scope.selected.preference);
+        $scope.search.major_art  = $scope.getTagId($scope.selected.major_art);
+        $scope.search.major_sci  = $scope.getTagId($scope.selected.major_sci);
+        $scope.search.industry   = $scope.getTagId($scope.selected.industry);
+        $scope.search.sex        = $scope.getTagId($scope.selected.sex);
+        $scope.search.operator   = $scope.getTagId($scope.selected.operator);
+        $scope.search.status     = $scope.getTagId($scope.selected.status);
+        $scope.search.loyalty    = $scope.selected.loyalty;
+         $scope.search.keyword        = $scope.selected.keyword;
+    };
+
+    $scope.filterByUniv = function(user) {
+        if($scope.search.univ != ""){ //選択されたタグが""（全て表示）でなければ絞り込みを行う．
+            return user.user_tag.indexOf($scope.search.univ) != -1; //array.indexOf(引数)はarrayに引数を含んでいればそのindex番号を返す．なければ-1を返す．
+        }else{
+            return -1; //filterを無効にしたいときは，戻り値に-1を指定すればよい。全て表示で用いる．
+        }
+    };
+
+    $scope.filterByGrade = function(user) {
+        if($scope.search.grade != ""){ //選択されたタグが""（全て表示）でなければ絞り込みを行う．
+            return user.user_tag.indexOf($scope.search.grade) != -1; //array.indexOf(引数)はarrayに引数を含んでいればそのindex番号を返す．なければ-1を返す．
+        }else{
+            return -1; //filterを無効にしたいときは，戻り値に-1を指定すればよい。全て表示で用いる．
+        }
+    };
+
+    $scope.filterByPreference = function(user) {
+        if($scope.search.preference != ""){ //選択されたタグが""（全て表示）でなければ絞り込みを行う．
+            return user.user_tag.indexOf($scope.search.preference) != -1; //array.indexOf(引数)はarrayに引数を含んでいればそのindex番号を返す．なければ-1を返す．
+        }else{
+            return -1; //filterを無効にしたいときは，戻り値に-1を指定すればよい。全て表示で用いる．
+        }
+    };
+
+    $scope.filterByMajorArt = function(user) {
+        if($scope.search.major_art != ""){ //選択されたタグが""（全て表示）でなければ絞り込みを行う．
+            return user.user_tag.indexOf($scope.search.major_art) != -1; //array.indexOf(引数)はarrayに引数を含んでいればそのindex番号を返す．なければ-1を返す．
+        }else{
+            return -1; //filterを無効にしたいときは，戻り値に-1を指定すればよい。全て表示で用いる．
+        }
+    };
+
+    $scope.filterByMajorSci = function(user) {
+        if($scope.search.major_sci != ""){ //選択されたタグが""（全て表示）でなければ絞り込みを行う．
+            return user.user_tag.indexOf($scope.search.major_sci) != -1; //array.indexOf(引数)はarrayに引数を含んでいればそのindex番号を返す．なければ-1を返す．
+        }else{
+            return -1; //filterを無効にしたいときは，戻り値に-1を指定すればよい。全て表示で用いる．
+        }
+    };
+
+    $scope.filterByIndustry = function(user) {
+        if($scope.search.industry != ""){ //選択されたタグが""（全て表示）でなければ絞り込みを行う．
+            return user.user_tag.indexOf($scope.search.industry) != -1; //array.indexOf(引数)はarrayに引数を含んでいればそのindex番号を返す．なければ-1を返す．
+        }else{
+            return -1; //filterを無効にしたいときは，戻り値に-1を指定すればよい。全て表示で用いる．
+        }
+    };
+
+    $scope.filterBySex = function(user) {
+        if($scope.search.sex != ""){ //選択されたタグが""（全て表示）でなければ絞り込みを行う．
+            return user.user_tag.indexOf($scope.search.sex) != -1; //array.indexOf(引数)はarrayに引数を含んでいればそのindex番号を返す．なければ-1を返す．
+        }else{
+            return -1; //filterを無効にしたいときは，戻り値に-1を指定すればよい。全て表示で用いる．
+        }
+    };
+
+    $scope.filterByOperator = function(user) {
+        if($scope.search.operator != ""){ //選択されたタグが""（全て表示）でなければ絞り込みを行う．
+            return user.user_tag.indexOf($scope.search.operator) != -1; //array.indexOf(引数)はarrayに引数を含んでいればそのindex番号を返す．なければ-1を返す．
+        }else{
+            return -1; //filterを無効にしたいときは，戻り値に-1を指定すればよい。全て表示で用いる．
+        }
+    };
+
+    $scope.filterByStatus = function(user) {
+        if($scope.search.status != ""){ //選択されたタグが""（全て表示）でなければ絞り込みを行う．
+            return user.user_tag.indexOf($scope.search.status) != -1; //array.indexOf(引数)はarrayに引数を含んでいればそのindex番号を返す．なければ-1を返す．
+        }else{
+            return -1; //filterを無効にしたいときは，戻り値に-1を指定すればよい。全て表示で用いる．
+        }
+    };
+
+    $scope.filterByLoyalty = function(user) {
+            return user.loyalty >= $scope.search.loyalty;
+    };
+
+    $scope.filterByKeyword = function(user) {
+         if($scope.search.status != ""){ //選択されたタグが""（全て表示）でなければ絞り込みを行う．
+              return $user == $scope.search.keyword; //array.indexOf(引数)はarrayに引数を含んでいればそのindex番号を返す．なければ-1を返す．
+        }else{
+            return -1; //filterを無効にしたいときは，戻り値に-1を指定すればよい。全て表示で用いる．
+        }
+    };
+
+     $scope.getUserMessages = function(user){
+
+        var url = LINE_API_URL+"/user/"+user.id+"/message";
+        $http({
+          url: url,
+          method: "GET",
+          dataType: "json",
+        }).
+        success(function(data, status, headers, config) {
+          for(var i in data){
+            data[i]['datetime'] = timeConverter(data[i]['created_date']);
+          }
+
+          $scope.userMessages = data;
+          $scope.currentUser = user;
+          $scope.showMessage = true;
+          $scope.messageSent = false;
+        }).
+        error(function(data, status, headers, config) {
+
+        });  
+      };
+
+    $scope.displayedResult = function(display, limit) {
+        return result = Math.floor(display/limit) + 1;
+    };
+
+    $scope.onpaging = function(page){ 
+            $scope.begin = $scope.num * page;
+    };
+
+        function categoryMapper(cat){
+      switch (cat){
+        case "univ":
+          return "大学"
+          break;
+        case "sex":
+          return "性別"
+          break;
+        case "position":
+          return "志望ポジション"
+          break;     
+        case "preference":
+          return "志向性"
+          break;       
+        case "major_sci":
+          return "専攻（理系）"
+          break;       
+        case "major_art":
+          return "専攻（文系）"
+          break;     
+        case "location":
+          return "希望勤務地"
+          break;  
+        case "industry":
+          return "志望業界"
+          break;  
+        case "grade":
+          return "学年"
+          break;
+        case "intelligence":
+          return "所感"
+          break;    
+        case "operator":
+          return "担当者"
+          break;      
+        default:
+          return "その他"
+          break;  
       }
-
-      $scope.userMessages = data;
-      $scope.currentUser = user;
-      $scope.showMessage = true;
-      $scope.messageSent = false;
-    }).
-    error(function(data, status, headers, config) {
-
-    });  
-  };
-
-$scope.displayedResult = function(display, limit) {
-    return result = Math.floor(display/limit) + 1;
-};
-
-$scope.onpaging = function(page){ 
-        $scope.begin = $scope.num * page;
-};
-
-    function categoryMapper(cat){
-  switch (cat){
-    case "univ":
-      return "大学"
-      break;
-    case "sex":
-      return "性別"
-      break;
-    case "position":
-      return "志望ポジション"
-      break;     
-    case "preference":
-      return "志向性"
-      break;       
-    case "major_sci":
-      return "専攻（理系）"
-      break;       
-    case "major_art":
-      return "専攻（文系）"
-      break;     
-    case "location":
-      return "希望勤務地"
-      break;  
-    case "industry":
-      return "志望業界"
-      break;  
-    case "grade":
-      return "学年"
-      break;
-    case "intelligence":
-      return "所感"
-      break;    
-    case "operator":
-      return "担当者"
-      break;      
-    default:
-      return "その他"
-      break;  
-  }
-}
+    }
 }]);
 
 
