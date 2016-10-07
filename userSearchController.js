e78b17a7d5bc2d769e1b467c242776ab9be7a035
terashin777 <terashin0324@gmail.com>
2016-10-07 11:44:05 +0900

    lineUserList作成時の処理#編集

--- userSearchController.js
+++ userSearchController.js
@@ -123,87 +123,90 @@ angular.module('concierAdminApp',[])
         $scope.lineUserList = data; //ここにユーザーリストが入る
         $scope.numOfUser = Object.keys($scope.lineUserList).length;
         $scope.numOfAdd = Object.keys($scope.addList).length ;
-        for(var p = 0; p< $scope.numOfUser; p++){
-            for(var q = 0; q<$scope.numOfAdd; q++){
-                if($scope.addList[q]["category"] == "univ_level"){
-                    $scope.lineUserList[p][$scope.addList[q]["category"]] = 0;
+        for(var userIdx=0 ; userIdx<$scope.numOfUser ; userIdx++){
+            for(var addIdx=0; addIdx<$scope.numOfAdd ; addIdx++){
+                if($scope.addList[addIdx]["category"] == "univ_level"){
+                    $scope.lineUserList[userIdx][$scope.addList[addIdx]["category"]] = 0;
                 }
                 else{
-                $scope.lineUserList[p][$scope.addList[q]["category"]] = "";
+                    $scope.lineUserList[userIdx][$scope.addList[addIdx]["category"]] = "";
                 }
                 //↑if文のelseは忘れない。
             }
         }
-        //↑ユーザーリストの1人1人にaddlistのタグを追加する。
+        //↑ユーザーリストの1人1人にaddlistのタグを追加する（空のプロパティを用意する）。
+        //↑view側でソート項目と同じ順番に表示されるようにここでは明示的にループをまわして、順番どおりに配列が作られるようにしている。
+        //↑for～inだと順番が変わってしまう恐れがある。
         $scope.numOfTag = Object.keys($scope.userTag).length;
-        for(var user_idx = 0; user_idx < $scope.numOfUser ;user_idx++){
+        for(userIdx in $scope.lineUserList){
             //↑ユーザー1人1人にソート項目の要素を追加する。
-            for(var i = 0; i <  $scope.lineUserList[user_idx].user_tag.length; i++){
-            //↑ユーザーのタグの数だけループを回し、ユーザーの持つ1つ1つのタグIDがどんなタグであるかを判定する。
-                for(var j = 0; j < $scope.numOfTag; j++){
+            for(addIdx in $scope.addList){
+            //↑ソート項目の数だけループを回し、ソート項目の中にあるタグであるかどうか判定する。
+                for(tagIdx in $scope.userTag){
                 //↑タグの数だけループを回し、ユーザーの持つタグIDと一致するIDを持つタグを探す。
-                    if($scope.lineUserList[user_idx].user_tag[i] == $scope.userTag[j].id){
-                        for(var k = 0; k < $scope.numOfAdd; k++){
-                        //↑ソート項目の数だけループを回し、ソート項目の中にあるタグであるかどうか判定する。
-                            if($scope.userTag[j].category == "major_art" || $scope.userTag[j].category == "major_sci"){
-                                if($scope.userTag[j].name != ""){
-                                    $scope.lineUserList[user_idx]["major"] = $scope.userTag[j].name;
-                                }
-                            }
-                            else if($scope.userTag[j].category == $scope.addList[k].category ){
-                                if($scope.userTag[j].name != ""){
-                                    $scope.lineUserList[user_idx][$scope.addList[k].category] = $scope.userTag[j].name;
-                                    //↑インデックス番号からユーザーを指定し、条件と合致した$scope.addListのcategoryと同じキーを持つところに、条件と合致したuserTagを代入している。
-                                    if($scope.userTag[j].category == "univ"){
-                                        switch ($scope.userTag[j].id){
-                                            case 24:
-                                            case 26:
-                                            case 6:
-                                            //↑上から東大、京大、東工大のｉｄ
-                                                $scope.lineUserList[user_idx].univ_level = 10;
-                                                break;
-                                            case 29:
-                                            case 40:
-                                            case 3:
-                                            case 27:
-                                            case 4:
-                                            case 25:
-                                            case 5:
-                                            case 11:
-                                            case 28:
-                                            case 23:
-                                            case 180:
-                                            //↑上から一橋、早稲田、慶応、阪大、東北大、名大、九大、北大、神戸、筑波（下2つ）のｉｄ
-                                                $scope.lineUserList[user_idx].univ_level = 9;
-                                                break;
-                                            case 126:
-                                            case 30:
-                                            case 9:
-                                            case 103:
-                                            case 37:
-                                            case 110:
-                                            case 35:
-                                            case 122:
-                                            case 31:
-                                            case 39:
-                                            case 125:
-                                            //↑上から横国、上智、理科大、工学院、電通、明大、青学、立教大学、中央大学、法政大学（下2つ）のｉｄ
-                                                $scope.lineUserList[user_idx].univ_level = 8;
-                                                break;
-                                            case 119:
-                                            case 177:
-                                            //↑上から関西大学、立命館大学のｉｄ
-                                                $scope.lineUserList[user_idx].univ_level = 7;
-                                                break;
-                                            case 129:
-                                            case 179:
-                                            //↑上から日大、東洋大のｉｄ
-                                                $scope.lineUserList[user_idx].univ_level = 6;
-                                                break;
-                                            default:
-                                                $scope.lineUserList[user_idx].univ_level = 0;
-                                                break;
-                                        }
+                    if($scope.userTag[tagIdx]["category"].indexOf($scope.addList[addIdx]["category"] + '') == -1){
+                    //↑addIdx + ''でaddIdxを文字列（String型）に変換。
+                        continue;
+                         //↑タグのcategoryが現在回しているaddlistのcategoryと異なる場合は、以下の処理を実行しない。
+                         //↑indexOfを用いているのは、categoryがmajor_artまたはmajor_sciであるものを同じものとみなすため。
+                         //↑indexOfを用いれば他のcategoryと区別しなくてよくなるため、majorのときだけ分岐するif文がいらなくなる。
+                    }
+                    for(userTagIdx in $scope.lineUserList[userIdx].user_tag){
+                    //↑ユーザーのタグの数だけループを回し、ユーザーの持つ1つ1つのタグIDがどんなタグであるかを判定する。
+                        if($scope.lineUserList[userIdx].user_tag[userTagIdx] == $scope.userTag[tagIdx].id){
+                        //↑ユーザーの持つタグとタグリストのタグのidが一致するか判定し、一致すればユーザーのそれに応じたカテゴリーのプロパティに値を代入する。
+                            if($scope.userTag[tagIdx].name != ""){
+                                $scope.lineUserList[userIdx][$scope.addList[addIdx]["category"]] = $scope.userTag[tagIdx].name;
+                                //↑userTagのnameが空でなければ、lineUserListのそれぞれのカテゴリーのプロパティに値を代入する。
+                                if($scope.userTag[tagIdx].category == "univ"){
+                                    switch ($scope.userTag[tagIdx].id){
+                                        case 24:
+                                        case 26:
+                                        case 6:
+                                        //↑上から東大、京大、東工大のｉｄ
+                                            $scope.lineUserList[userIdx].univ_level = 10;
+                                            break;
+                                        case 29:
+                                        case 40:
+                                        case 3:
+                                        case 27:
+                                        case 4:
+                                        case 25:
+                                        case 5:
+                                        case 11:
+                                        case 28:
+                                        case 23:
+                                        case 180:
+                                        //↑上から一橋、早稲田、慶応、阪大、東北大、名大、九大、北大、神戸、筑波（下2つ）のｉｄ
+                                            $scope.lineUserList[userIdx].univ_level = 9;
+                                            break;
+                                        case 126:
+                                        case 30:
+                                        case 9:
+                                        case 103:
+                                        case 37:
+                                        case 110:
+                                        case 35:
+                                        case 122:
+                                        case 31:
+                                        case 39:
+                                        case 125:
+                                        //↑上から横国、上智、理科大、工学院、電通、明大、青学、立教大学、中央大学、法政大学（下2つ）のｉｄ
+                                            $scope.lineUserList[userIdx].univ_level = 8;
+                                            break;
+                                        case 119:
+                                        case 177:
+                                        //↑上から関西大学、立命館大学のｉｄ
+                                            $scope.lineUserList[userIdx].univ_level = 7;
+                                            break;
+                                        case 129:
+                                        case 179:
+                                        //↑上から日大、東洋大のｉｄ
+                                            $scope.lineUserList[userIdx].univ_level = 6;
+                                            break;
+                                        default:
+                                            $scope.lineUserList[userIdx].univ_level = 0;
+                                            break;
                                     }
                                 }
                             }
@@ -430,6 +433,78 @@ angular.module('concierAdminApp',[])
     };
 */
 
+    $scope.filterByTag = function(user){
+    //↓フィルターごとではなく、ユーザーごとにフィルターの処理を実行しなくては、動作が遅くなる。
+    //↓フィルターごとにタグリストを参照するループ関数が含むことになるため。
+        for(tagGroup in $scope.search)
+            if(tagGroup == "univ_level"){
+                for(item in $scope.search[tagGroup]){
+                    if($scope.search[tagGroup][item]){
+                        if(!$scope.nullTagUserFrags[tagGroup] && user[tagGroup] != item){
+                            return false;
+                            //↑lineUserListのなか"univ_level"が一致するものでフィルターをかけている。
+                            //↑選択されたuniv_levelのどれかと一致しない、かつタグなし含むにチェックが入れられていない時はfalseを返してそのユーザーをはじく。
+                            //↑タグの分類ごとにreturn false;が設定されているので、AND検索。
+                            //↑どれか一つの分類でfalseが返されたら、そのユーザーをはじく。
+                            //↑どの分類でもfalseを返されずに最後まで残ったユーザーに対してのみtrueが返されて、表示される。
+                        }
+                    }
+                }
+            }
+            else if(tagGroup == "major"){
+                for(item in $scope.search[tagGroup]){
+                    if($scope.search[tagGroup][item]){
+                    //↑個別のタグがtrue（チェックされている）なら判定を行う。
+                        //↓文系タグのフィルター
+                        if(item == "文系"){
+                        //↑文系にチェックが入れられているときは別処理を行う。
+                            for(tag in $scope.userTag){
+                                if($scope.userTag[tag].category == "major_art"){
+                                //↑タグリストのなかの文系タグだけを全て取り出してフィルターをかける。
+                                    if(user.user_tag.indexOf($scope.userTag[tag].id) != -1){ 
+                                    //↑array.indexOf(引数)はarrayに引数を含んでいればそのindex番号を返す．なければ-1を返す．
+                                    //↑-1を返さない。つまり、arrayに引数を含んでいるという条件でfilterをかけている。
+                                        return true;
+                                    //↑選択されたタグの一つでも一致する時は、そのユーザーを残す。
+                                    }
+                                }
+                            }
+                        }
+                        //↓理系タグのフィルター
+                        else{
+                            if(user[tagGroup] == item){ 
+                            //↑array.indexOf(引数)はarrayに引数を含んでいればそのindex番号を返す．なければ-1を返す．
+                            //↑-1を返さない。つまり、arrayに引数を含んでいるという条件でfilterをかけている。
+                                return true;
+                            }
+                        }
+                    }
+                }
+                if($scope.nullTagUserFrags[tagGroup]){
+                //↑タグなしユーザーを含むにチェックが入れられていて、かつタグを持っていない時はそのユーザーを残す。
+                    if(user[tagGroup] == ""){
+                        return true;
+                    }
+                }
+                    return false;
+                }
+            else{
+                for(item in $scope.search[tagGroup]){
+                    if($scope.search[tagGroup][item]){
+                        if(user[tagGroup] == item){
+                            return true;
+                            //↑lineUserListのなか"univ_level"が一致するものでフィルターをかけている。
+                            //↑選択されたuniv_levelのどれかと一致するユーザーは残す。
+                        }
+                    }
+                }
+                if($scope.nullTagUserFrags[tagGroup] && user[tagGroup] == ""){
+                //↑タグなしユーザーを含むにチェックが入れられていて、かつタグを持っていない時はそのユーザーを残す。
+                    return true;
+                }
+                return false;
+            }
+    }
 
     $scope.filterByUnivLevel = function(user) {
         var tagGroup = "univ_level";
@@ -447,7 +522,7 @@ angular.module('concierAdminApp',[])
             return true;
         }
         return false;
-    };
+    }
 
     $scope.filterByGrade = function(user) {
         var tagGroup = "grade";
